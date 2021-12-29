9ef60a61f241fd1ad953b3eda0516f6cdab3ae54
Sergio Rosa <scarlosjazz@gmail.com>
2021-12-29 10:29:04 -0300

    Var, let e const

---
+++ var-let-const/JS-Types/script.js
@@ -0,0 +1,22 @@
+const x = 10;
+
+if (x > 0) {
+ var a = 100;
+ let b = 200;
+ const c = 300;
+ 
+ console.log("printing inside of the if:");
+ console.log(a);
+ console.log(b);
+ console.log(c);
+}
+
+ console.log("printing outside of the if:");
+ console.log(a);
+ // console.log(b);
+ // console.log(c);
+
+ console.log("Print for result:");
+ for(let i = 0; i < 5; i++){
+  console.log(i);
+ }
